eb4692c43c31addc2b312493a4802ce72bd7fb79
Likhitha Sunkara <likhithasunkara116@gmail.com>
2025-12-24 20:17:41 +0530

    1   1 2   1 2 3   1 2 3 4   1 2 3 4 5

---
+++ Patterns/pattern3.py
@@ -0,0 +1,23 @@
+def patterns(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j, end =" ")
+        print("\n")
+
+for _ in range(int(input("Enter no.of times: "))):
+    n = int(input("Enter value of n: "))
+    patterns(n)
+
+
+output:
+Enter no.of times: 1
+Enter value of n: 5
+1 
+
+1 2 
+
+1 2 3 
+
+1 2 3 4 
+
+1 2 3 4 5 
