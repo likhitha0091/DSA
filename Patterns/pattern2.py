59f899700b353f851b5438766b0898c9a26a6459
Likhitha Sunkara <likhithasunkara116@gmail.com>
2025-12-24 20:10:43 +0530

    pattern2.py

--- Patterns/pattern2.py
+++ Patterns/pattern2.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+def patterns(n):
+    for i in range(n):
+        for j in range(i):
+            print("*", end =" ")
+        print("\n")
+
+for _ in range(int(input("Enter no.of times: "))):
+    n = int(input("Enter value of n: "))
+    patterns(n)
