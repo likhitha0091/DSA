2b6224fb65285e75ffe4fd9f8aa946c7049f6374
Likhitha Sunkara <likhithasunkara116@gmail.com>
2025-12-27 14:14:02 +0530

    revNumber.py

---
+++ Math/revNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseDigits(self, n):
+        rev = 0
+        while n>0:
+            digit = n%10
+            rev = (rev *10)+digit
+            n = n//10
+        return rev
+        
