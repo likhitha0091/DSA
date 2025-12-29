ebbd76a43d553c2023d563bc709892a9dad693d8
Likhitha Sunkara <likhithasunkara116@gmail.com>
2025-12-29 10:35:35 +0530

    LargestElement.py

--- Arrays/LargestElement.py
+++ Arrays/LargestElement.py
@@ -1,7 +1,7 @@
 class Solution:
     def largest(self, arr):
         # code here
-      # Method1
+      # Method1(Optimal-TC->O(N))
         largest = arr[0]
         for i in range(len(arr)):
            if arr[i]>largest:
@@ -9,7 +9,7 @@ class Solution:
         return largest
 
 
-       # Method2
+       # Method2(BruteForce-TC->O(NlogN))
         arr.sort()
         return arr[len(arr)-1]
         
